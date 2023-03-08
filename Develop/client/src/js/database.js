a22779ae17c0c0d8fcf9602019de867b59509f8b
yerimechoi <yerimechoi@gmail.com>
2023-03-07 23:13:44 -0500

    Fixed id and registerRoute error

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -19,9 +19,10 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('result.value', result);
+  return result?.value;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -31,11 +32,10 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  // return result
-  return "Hello World"
+  return result?.value;
 };
 
 initdb();
