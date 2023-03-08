7965e9bc02f3baa98d4003fe725915d91645a85b
yerimechoi <yerimechoi@gmail.com>
2023-03-07 23:11:45 -0500

    Changed contact name to JATE

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -28,9 +28,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
